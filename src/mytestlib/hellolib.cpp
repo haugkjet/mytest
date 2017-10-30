dac2afbd72f31590fe420ff24e46f0bc009464ba
Kjetil Haugland <kh@rds.no>
2017-10-30 20:22:26 +0100

    Added NDDS, not working yet

--- src/mytestlib/hellolib.cpp
+++ src/mytestlib/hellolib.cpp
@@ -2,9 +2,14 @@
 
 #include "../../inc/shared_EXPORTS.h"
 
+#include "ndds_cpp.h"
+
 void shared_EXP printlib()
 {
-   std::cout << "Print Lib4" << std::endl;
+    DDS_ReturnCode_t         rc;
+    DDSDomainParticipant *   participant = NULL;
+    DDSTopic *               topic = NULL;
+    std::cout << "Print Lib4" << std::endl;
 }
 
 int shared_EXP getnumberfromlib()
